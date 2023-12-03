eb3f8cb4f92894d22b08e4d902812722b1451a02
Egor Marchenko <egorfing@yandex.ru>
2023-12-03 15:42:49 +0530

    new theme

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,23 +1,34 @@
-import { useContext } from 'react';
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+import { useContext } from 'react'
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext'
 
 interface UseThemeResult {
-    toggleTheme: () => void;
-    theme: Theme;
+  toggleTheme: () => void
+  theme: Theme
 }
 
 export function useTheme(): UseThemeResult {
-    const { theme, setTheme } = useContext(ThemeContext);
+  const { theme, setTheme } = useContext(ThemeContext)
 
-    const toggleTheme = () => {
-        const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
-        setTheme?.(newTheme);
-        document.body.className = newTheme
-        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
-    };
+  const toggleTheme = () => {
+    let newTheme: Theme
+    switch (theme) {
+      case Theme.DARK:
+        newTheme = Theme.LIGHT
+        break
+      case Theme.LIGHT:
+        newTheme = Theme.ORANGE
+        break
+      default:
+        newTheme = Theme.DARK
+        break
+    }
+    setTheme?.(newTheme)
+    document.body.className = newTheme
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+  }
 
-    return {
-        theme: theme || Theme.LIGHT,
-        toggleTheme,
-    };
+  return {
+    theme: theme || Theme.LIGHT,
+    toggleTheme
+  }
 }
